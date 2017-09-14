2995f3b3da7660e7d364d3d1edb2ce15b2c2c6c9
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-09-14 09:59:06 +0200

    ex 3.6 init

--- src/main/scala/ch3/List.scala
+++ src/main/scala/ch3/List.scala
@@ -37,4 +37,10 @@ object List {
     case Cons(h, t) if p(h) => dropWhile(t)(p)
     case t => t
   }
+
+  def init[A](ls: List[A]): List[A] = ls match {
+    case Nil => Nil
+    case Cons(_, Nil) => Nil
+    case Cons(h, t) => Cons(h, init(t))
+  }
 }
