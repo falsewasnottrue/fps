ad6a597f6affbfceb4fbc46bec4a18a55b17082c
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-09-18 08:58:21 +0200

    ex 4.1 option flatMap

--- src/main/scala/ch4/Option.scala
+++ src/main/scala/ch4/Option.scala
@@ -2,10 +2,12 @@ package ch4
 
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = None
+  def flatMap[B](f: A => Option[B]): Option[B] = None
 }
 
 case class Some[+A](get: A) extends Option[A] {
   override def map[B](f: A => B): Option[B] = Some(f(get))
+  override def flatMap[B](f: A => Option[B]): Option[B] = f(get)
 }
 
 case object None extends Option[Nothing]
--- src/test/scala/ch4/OptionSpec.scala
+++ src/test/scala/ch4/OptionSpec.scala
@@ -9,7 +9,16 @@ class OptionSpec extends FlatSpec with Matchers {
     Some(1).map(_ + 1) should be(Some(2))
   }
 
-  // flatMap
+  "flatMap" should "flatMap value if it is present" in {
+    Some(1).flatMap(i => Some(i * 3)) should be(Some(3))
+    Some(1).flatMap(i => None) should be(None)
+  }
+
+  it should "return None if there is no value" in {
+    (None: Option[Int]).flatMap(_ => Some(1)) should be(None)
+    (None: Option[Int]).flatMap(_ => None) should be(None)
+  }
+
   // getOrElse
   // orElse
   // filter
