cf810f8099d4cb1b63c79d6b58760eff8ee62567
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-09-19 13:46:21 +0200

    ex 4.6 either flatMap

--- src/main/scala/ch4/Either.scala
+++ src/main/scala/ch4/Either.scala
@@ -6,7 +6,10 @@ sealed trait Either[+E, +A] {
     case Right(a) => Right(f(a))
     case Left(e) => Left(e)
   }
-  def flatMap[EE >: E, B](f: A => Either[EE,B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE,B]): Either[EE, B] = this match {
+    case Right(a) => f(a)
+    case Left(e) => Left(e)
+  }
   def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A,B) => C): Either[EE, C] = ???
 }
--- src/test/scala/ch4/EitherSpec.scala
+++ src/test/scala/ch4/EitherSpec.scala
@@ -12,4 +12,15 @@ class EitherSpec extends FlatSpec with Matchers {
     val v: Either[String, Int] = Left("error")
     v.map(_ + 1) should be(Left("error"))
   }
+
+  "flatMap" should "work on right value" in {
+    Right(1).flatMap(i => Right(i+1)) should be(Right(2))
+    Right(1).flatMap(_ => Left("error")) should be(Left("error"))
+  }
+
+  it should "work on left value" in {
+    val v: Either[String, Int] = Left("error")
+    v.flatMap(i => Right(i+1)) should be(v)
+    v.flatMap(_ => Left("error 2")) should be(Left("error"))
+  }
 }
