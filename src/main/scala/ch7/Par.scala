ddf61bc65be2342b7cb224951d3f1dbc61bc74e3
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-10-03 11:53:49 +0200

    ex 7.2 par implemented via executorService

--- src/main/scala/ch7/Par.scala
+++ src/main/scala/ch7/Par.scala
@@ -1,15 +1,34 @@
 package ch7
 
-class Par[A] {
-
-}
-
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
 
 object Par {
 
-  def unit[A](a: => A): Par[A] = ???
+  type Par[A] = ExecutorService => Future[A]
 
-  def get[A](a: Par[A]): A = ???
+  def unit[A](a: => A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
-  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    override def isDone: Boolean =  true
+    override def get(timeout: Long, unit: TimeUnit): A = get
+    override def isCancelled: Boolean = false
+    override def cancel(mayInterruptIfRunning: Boolean): Boolean = false
+  }
+
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      override def call(): A = a(es).get
+    })
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 }
