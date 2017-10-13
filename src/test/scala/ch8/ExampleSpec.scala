929071bd1f4d45a1419a16b6c49b9dee7915f6ba
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-10-13 11:35:49 +0200

    ex 8.13 gen.listOf1

--- src/test/scala/ch8/ExampleSpec.scala
+++ src/test/scala/ch8/ExampleSpec.scala
@@ -8,11 +8,9 @@ class ExampleSpec extends FlatSpec with Matchers {
 
   "an example" should "work" in {
     val smallInts = Gen.choose(-10, 10)
-    val maxProp = forAll(listOf(smallInts)) { ns =>
-      if (ns.isEmpty) true else {
-        val max = ns.max
-        !ns.exists(_ > max)
-      }
+    val maxProp = forAll(listOf1(smallInts)) { ns =>
+      val max = ns.max
+      !ns.exists(_ > max)
     }
 
     Prop.run(maxProp)
