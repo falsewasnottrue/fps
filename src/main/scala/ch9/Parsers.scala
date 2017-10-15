9cbd5f35ca4b0ff31a6c94900f5be742e089a431
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-10-15 15:28:16 +0200

    some initial laws & more implicit converters

--- src/main/scala/ch9/Parsers.scala
+++ src/main/scala/ch9/Parsers.scala
@@ -1,18 +1,38 @@
 package ch9
 
+import ch8.Gen
+
 trait Parsers[ParseError, Parser[+_]] { self =>
+
+  def map[A,B](pa: Parser[A])(f: A => B): Parser[B]
 
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
   def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+  def many[A](pa: Parser[A]): Parser[List[A]]
 
   implicit def char(c: Char): Parser[Char]
   implicit def string(s: String): Parser[String]
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] =
     ParserOps(f(a))
 
-  case class ParserOps[A](p: Parser[A]) {
+  implicit class ParserOps[A](p: Parser[A]) {
     def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
     def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+
+    def many = self.many(p)
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+  }
+
+  object Laws {
+    import ch8.Prop._
+
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(a => a))(in)
   }
 }
