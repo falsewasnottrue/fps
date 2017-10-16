03dadbdf698c5f969747d812d1cd738f1c7c36dc
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-10-16 13:14:15 +0200

    JSON parser scaffold

--- src/main/scala/ch9/Parsers.scala
+++ src/main/scala/ch9/Parsers.scala
@@ -7,16 +7,17 @@ trait Parsers[ParseError, Parser[+_]] { self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
   def map[A,B](pa: Parser[A])(f: A => B): Parser[B]
-  def map2[A,B,C](pa: Parser[A], pb: Parser[B])(f: (A,B) => C): Parser[C] = (pa ** pb).map(f(_))
+  def flatMap[A,B](pa: Parser[A])(f: A => Parser[B]): Parser[B]
+  def map2[A,B,C](pa: Parser[A], pb: => Parser[B])(f: (A,B) => C): Parser[C] = (pa ** pb).map(f(_))
 
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
   def slice[A](p: Parser[A]): Parser[String]
-  def many1[A](pa: Parser[A]): Parser[List[A]]
-  def product[A,B](pa: Parser[A], pb: Parser[B]): Parser[(A,B)]
+  def product[A,B](pa: Parser[A], pb: => Parser[B]): Parser[(A,B)]
 
   def succeed[A](a: A): Parser[A] = string("").map(_ => a)
   def many[A](pa: Parser[A]): Parser[List[A]] =
     map2(pa, many(pa))(_ :: _) or succeed(List())
+  def many1[A](pa: Parser[A]): Parser[List[A]] = ???
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] =
     if (n==0) succeed(List()) else map2(p, listOfN(n-1, p))(_ :: _)
 
@@ -31,6 +32,7 @@ trait Parsers[ParseError, Parser[+_]] { self =>
 
     def many = self.many(p)
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
     def **[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
     def product[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
   }
