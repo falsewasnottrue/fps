ecd143d6e627106ae7d278cb682e15fe6e43b1a5
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-10-17 09:13:56 +0200

    implementation for string monoid and list monoid+

--- src/main/scala/ch10/Monoid.scala
+++ src/main/scala/ch10/Monoid.scala
@@ -4,3 +4,21 @@ trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
 }
+
+object Monoid {
+
+  val stringMonoid: Monoid[String] = new Monoid[String] {
+    override def op(a1: String, a2: String) = a1 + a2
+    override def zero = ""
+  }
+
+  def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
+    override def op(a1: List[A], a2: List[A]) = a1 ++ a2
+    override def zero = Nil
+  }
+
+  // intAddition
+  // intmultiplication
+  // booleanOr
+  // booleanAnd
+}
