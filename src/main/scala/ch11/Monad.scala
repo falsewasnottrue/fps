0a61a099e74ca95df6c2b575810b17b5d0fd5cbe
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-10-19 13:13:36 +0200

    monad trait

---
+++ src/main/scala/ch11/Monad.scala
@@ -0,0 +1,13 @@
+package ch11
+
+trait Monad[M[_]] extends Functor[M] {
+
+  def unit[A](a: => A): M[A]
+  def flatMap[A,B](ma: M[A])(f: A => M[B]): M[B]
+
+  override def map[A, B](fa: M[A])(f: A => B) =
+    flatMap(fa)(a => unit(f(a)))
+
+  def map2[A,B,C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
+    flatMap(ma)(a => map(mb)(b => f(a,b)))
+}
